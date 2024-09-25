232c33aef1d5d5e7afbfe9fc2e8e7e339466efd5
ralamo <laborda.ramon@gmail.com>
2024-09-25 20:20:19 +0200

    Añado Ejercicio 11

---
+++ Ejercicio 11.sql
@@ -0,0 +1,21 @@
+WITH tmp_table AS (
+  SELECT ivr_calls.ivr_id
+       , ivr_calls.phone_number
+       , ivr_calls.start_date
+       , LAG(ivr_calls.start_date) OVER (PARTITION BY phone_number ORDER BY ivr_calls.start_date) AS prev_call_time
+       , LEAD(ivr_calls.start_date) OVER (PARTITION BY phone_number ORDER BY ivr_calls.start_date) AS next_call_time
+  FROM `keepcoding.ivr_calls` ivr_calls
+)
+SELECT  tmp_table.ivr_id AS calls_ivr_id
+      , CASE WHEN tmp_table.prev_call_time IS NOT NULL AND TIMESTAMP_DIFF(tmp_table.start_date, tmp_table.prev_call_time, HOUR) <= 24 THEN 1 ELSE 0
+        END flag_llamada_ultimas_24_horas
+      , CASE WHEN next_call_time IS NOT NULL AND TIMESTAMP_DIFF(tmp_table.next_call_time, tmp_table.start_date, HOUR) <= 24 THEN 1 ELSE 0
+        END flag_llamada_siguientes_24_horas
+FROM tmp_table;
+
+/*Primero creamos tabla temporal que utiliza el registro y el numero de telefono ademas de la fecha de entrada de llamada
+para crear 2 nuevas columnas que con el numero de telefono (no el id) me indique la hora de la llamada anterior y la siguiente
+
+Se usa solo la tabla de llamadas y aplicamos una llamada a la tabla temporal con un case when donde la diferencia de tiempo es inferior a 24 H*/
+
+# 3 columnas con 21674 registros
